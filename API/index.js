a5296b7cec184bc88cfc4de27b5dc94440270d2e
Jacob Guirguis <jacob.guirguis@gmail.com>
2020-01-26 02:18:55 -0500

    yessir, we got it baby

--- API/index.js
+++ API/index.js
@@ -1,37 +1,66 @@
 const { WebClient } = require('@slack/web-api');
-const token = process.env.d62a1452f835983e66fcff1c3cad4681;
-const web = new WebClient(token);
+const { createEventAdapter } = require('@slack/events-api');
 const https = require('https');
+const token = process.env.d62a1452f835983e66fcff1c3cad4681;
+const signingSecret = 'd62a1452f835983e66fcff1c3cad4681';
+const web = new WebClient(token);
+const app = require('express')();
+const fs = require('fs');
+
+const slackEvents = createEventAdapter(signingSecret);
+
+app.use('/slack/events', slackEvents.expressMiddleware());
+
+slackEvents.on('message.channels', (event) => {
+    console.log('Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}');
+});
+
+slackEvents.on('message.groups', (event) => {
+    console.log('Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}');
+});
+
+slackEvents.on('message.im', (event) => {
+    console.log('Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}');
+});
+
+slackEvents.on('message.mpim', (event) => {
+    console.log('Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}');
+});
 
 exports.helloWorld = (req, res) => {
-    //sendMessage();
-    receiveNewMessages();
+    // const slackEvents = createEventAdapter(signingSecret);
+
+    // app.use('/slack/events', slackEvents.expressMiddleware());
+
+    // slackEvents.on('message.channels', (event) => {
+    //     console.log('Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}');
+    // });
+
+
+    // //sendMessage();
     res.send('Hello, World');
+
+
 };
+
+exports.users = app;
 
 function sendMessage() {
     var postBody = {
-        token: 'xoxp-881911229938-920885728916-909500976514-5a50c71b1264f64c919fd2f318c10aaa',
+        token: 'xoxp-881911229938-920885728916-924555651494-0f20704889a75981c12cd3a2cd086bfd',
         channel: 'GSRFH8D5Y',
-        text: 'test',
+        text: 'TESTTTTT',
         as_user: 'true'
     }
 
     const res = web.chat.postMessage(postBody);
 }
 
-function receiveNewMessages() {
-    var postBody = {
-        client_id : '881911229938.910783800067',
-        scope : 'users:read'
-    }
-}
 
-function authorization() {
-    var postBody = {
-        
-    }
-}
+
+
+
+
 
 
 
