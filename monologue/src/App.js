466b80dc6391bc27c66f45e5ca27c3f066e87a0b
Sharon Ho <sharon.cy.ho@gmail.com>
2020-01-26 01:33:07 -0500

    Music player and autoplay  image carousel

--- monologue/src/App.js
+++ monologue/src/App.js
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState,useEffect} from 'react';
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+import AliceCarousel from 'react-alice-carousel'
+import 'react-alice-carousel/lib/alice-carousel.css'
 import './App.css';
 
 function App() {
+
+  const song1 = "https://conuhacks-2020.tsp.cld.touchtunes.com/v1/songs/7782908";
+  const song2 = "https://conuhacks-2020.tsp.cld.touchtunes.com/v1/songs/62435015";
+  const song3 = "https://conuhacks-2020.tsp.cld.touchtunes.com/v1/songs/62435011";
+  const img1 = "https://image.shutterstock.com/z/stock-vector-innovations-systems-connecting-people-and-robots-devices-future-technologies-in-automatics-cyborg-1061543018.jpg";
+  const img2 = "https://image.shutterstock.com/z/stock-vector-touch-the-future-interface-technology-the-future-of-user-experience-503854255.jpg";
+  const img3 = "https://image.shutterstock.com/z/stock-photo-woman-with-glasses-of-virtual-reality-future-technology-concept-528831091.jpg";
+  const songs = [song1,song2,song3]
+  const galleryItems = [img1, img2, img3]
+
+  const [musiccode, setmusiccode] = useState(0);
+  const [music, setmusic] = useState("");
+  const [musicName, setmusicName] = useState("")
+
+  const fetchSongs=async(url)=>{
+    await fetch(`https://cors-anywhere.herokuapp.com/${url}`,{
+       method:"GET",
+       headers:{
+         "Authorization":"u5bm4v4ivl1o7v6n5v2mrw08kn6y9fdp"
+       }
+     }).then(res=>{
+       res.json().then((data)=>{
+         setmusicName(data.title)
+         setmusic(data.playUrl)
+       })
+     })
+   };
+
+ const handlePrev=(e) =>{
+  e.preventDefault();
+    if(musiccode == 0){
+      setmusiccode(2)
+    }else{
+      let newcode = musiccode-1;
+      setmusiccode(newcode)
+    }
+    fetchSongs(songs[musiccode]);
+  }
+
+  const handleNext = e =>{
+    e.preventDefault();
+    if(musiccode == 2){
+      setmusiccode(0)
+    }else{
+      let newcode = musiccode+1;
+      setmusiccode(newcode)
+    }
+    fetchSongs(songs[musiccode]);
+  }
+
+  useEffect(()=>(fetchSongs(songs[musiccode])),[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="center-fit">
+      <AliceCarousel
+        autoPlayInterval={2000}
+        autoPlayDirection="rtl"
+        autoPlay={true}
+        fadeOutAnimation={true}
+        dotsDisabled={true}
+        buttonsDisabled={true}
+        autoHeight={true}
+      >
+        <img src={galleryItems[0]} />
+        <img src={galleryItems[1]} />
+        <img src={galleryItems[2]} />
+      </AliceCarousel>
+      <AudioPlayer
+        autoPlay
+        src={music}
+        onPlay={e => console.log("onPlay")}
+        onClickPrevious={handlePrev}
+        showSkipControls = {true}
+        onEnded = {handleNext}
+        onClickPrevious = {handlePrev}
+        onClickNext = {handleNext}
+        // other props here
+      />
     </div>
-  );
+  )
 }
 
 export default App;
